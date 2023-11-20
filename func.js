c7f7b70f380f2309f32db0bf76b3f75e9128a4aa
WrongProvider <guara.taste@gmail.com>
2023-11-20 12:10:23 -0300

    add links, new js function to change links and innerhtml

--- func.js
+++ func.js
@@ -9,9 +9,9 @@ function toggleMode (){
   //   html.classList.add('light')
   // }
 
-  const img = document.querySelector("#profile img")
+  const img = document.querySelector('#profile img')
 
-  //substitui alguns elementos da pagina
+  //substitui a foto de profile da pagina
   if (html.classList.contains('light')){
     img.setAttribute('src', './assets/happy_guitar.png')
   }
@@ -19,4 +19,13 @@ function toggleMode (){
     img.setAttribute('src', './assets/me_dev_format.png')
   }
   
+  const listas = document.querySelector('ul li a')
+
+  if (html.classList.contains('light')){
+    listas.innerHTML = '"Música é a maior forma de expressão metafísica do ser humano"'
+    listas.setAttribute('href','https://www.instagram.com/p/Cy4GqyRrXxj/?img_index=1')
+  }
+  else{
+    listas.setAttribute('href','https://tryhackme.com/p/liquidsmoke')
+  }
 }
